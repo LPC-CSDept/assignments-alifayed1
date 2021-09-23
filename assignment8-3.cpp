8c64c369c702f7e19dbc9b3015223502942fc98f
ALI FAYED <72723479+alifayed1@users.noreply.github.com>
2021-09-23 19:31:19 +0000

    Indentation

--- assignment8-3.cpp
+++ assignment8-3.cpp
@@ -7,7 +7,7 @@ void printArray(int [], int);
 int main()
 {
 	const int N = 10;
-	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55,20,50};
+	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
 	int minidx;
 
 	printArray(numbers, N);
