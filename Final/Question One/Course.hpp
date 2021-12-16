1b8999b373de896934652a8c715a3c350a4813d5
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-15 20:13:16 -0800

    Completed Course class design

--- Final/Question One/Course.hpp
+++ Final/Question One/Course.hpp
@@ -1,10 +1,26 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <string>
+
+using namespace std;
+
 class Course {
+    private:
+        int id;
+        string name;
+        int credits;
+    public:
+        Course(int, string, int);
+        ~Course();
 
-    
+        int getID() const;
+        string getName() const;
+        int getCredits() const;
 
+        void setID(int);
+        void setName(string);
+        void setCredits(int);
 };
 
 #endif
