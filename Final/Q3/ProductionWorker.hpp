9c6bbaac4d09186cfe4e6e8a77a3a4c67345741d
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-16 00:02:34 -0800

    basic Production Worker header file

---
+++ Final/Q3/ProductionWorker.hpp
@@ -0,0 +1,25 @@
+#ifndef PRODUCTIONWORKER_H
+#define PRODUCTIONWORKER_H
+
+#include "Employee.hpp"
+#include <string>
+
+using namespace std;
+
+class ProductionWorker : public Employee {
+    private:
+        int shift;
+        double hourlyWage;
+    public:
+        ProductionWorker();
+        ProductionWorker(int, double); 
+        ~ProductionWorker();
+
+        int getShift() const;
+        double getHourlyWage() const;
+
+        void setShift(int);
+        void setHourlyWage(double);
+};
+
+#endif
