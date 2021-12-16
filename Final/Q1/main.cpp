ce720f3db80e0b8084928af489a9390b5efc9cba
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-15 21:18:46 -0800

    Implemented iterative binary search

--- Final/Q1/main.cpp
+++ Final/Q1/main.cpp
@@ -4,18 +4,30 @@
 
 using namespace std;
 
-void fillCourseArray(Course*, int);
+void fillCourseArray(Course[], int);
+int binarySearch(Course[], int, int);
 
 int main() {
     int size = 10;
     Course courses[size];
 
     fillCourseArray(courses, size);
-    
+
+    cout << "Index\tID\tName\n\n";
+    for(int i = 0; i < size; i++) {
+        cout << i << "\t" << courses[i].getID() << "\t" << courses[i].getName() << endl;
+    }
+    cout << endl;
+
+    int target = 103;
+    cout << "Locating " << target << "...\n";
+    int index = binarySearch(courses, size, target);
+    cout << "Found " << target << " at: " << index << endl;
+
     return 0;
 }
 
-void fillCourseArray(Course* courses, int size) {
+void fillCourseArray(Course courses[], int size) {
     ifstream ifs;
     ifs.open("C:\\Users\\alifa\\Documents\\CS2 Assignments\\assignments-alifayed1\\Final\\Q1\\courses.txt");
 
@@ -27,4 +39,22 @@ void fillCourseArray(Course* courses, int size) {
     for(int i = 0; i < size; i++) {
         ifs >> courses[i];
     }
+}
+
+int binarySearch(Course courses[], int size, int target) {
+    int first, last, mid;
+    first = 0;
+    last = size-1;
+
+    while(first <= last) {
+        mid = last + ((first - last) / 2); // Safe way to calculate mid to prevent overflow
+        if(courses[mid].getID() == target) {
+        return mid;
+        } else if(courses[mid].getID() > target) {
+        last = mid - 1;
+        } else {
+        first = mid + 1;
+        }
+    }
+    return -1;
 }
