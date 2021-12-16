d58865c90e19d6b4f5d24b23164fb0c297fed171
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-15 23:52:24 -0800

    Created Employee header file

---
+++ Final/Q3/Employee.hpp
@@ -0,0 +1,27 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+
+using namespace std;
+
+class Employee {
+    private:
+        string name;
+        int number;
+        string hireDate;
+    public:
+        Employee();
+        Employee(string, int, string);
+        ~Employee();
+
+        string getName() const;
+        int getNumber() const;
+        string getHireDate() const;
+
+        void setName(string);
+        void setNumber(int);
+        void setHireDate(string);
+};
+
+#endif
