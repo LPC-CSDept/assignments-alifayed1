132cda4802b791c5d97bcd7466a335e154383da4
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-15 20:17:00 -0800

    Completed header file implementation

---
+++ Final/Question One/Course.cpp
@@ -0,0 +1,35 @@
+#include "Course.hpp"
+
+Course::Course(int setID, string setName, int setCredits) {
+    id = setID;
+    name = setName;
+    credits = setCredits;
+}
+
+Course::~Course() {
+
+}
+
+int Course::getID() const {
+    return id;
+}
+
+string Course::getName() const {
+    return name;
+}
+
+int Course::getCredits() const {
+    return credits;
+}
+
+void Course::setID(int newID) {
+    id = newID;
+}
+
+void Course::setName(string newName) {
+    name = newName;
+}
+
+void Course::setCredits(int newCredits) {
+    credits = newCredits;
+}
