5eaaed41abdc3318713f6443f24ec4ff2836dc3a
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-15 21:38:50 -0800

    Copied Course files over

---
+++ Final/Q2/Course.cpp
@@ -0,0 +1,55 @@
+#include "Course.hpp"
+
+using namespace std;
+
+Course::Course() {
+    id = 0;
+    name = "";
+    credits = 0;
+}
+
+Course::Course(int setID, string setName, int setCredits) {
+    id = setID;
+    name = setName;
+    credits = setCredits;
+}
+
+Course::~Course() {
+
+}
+
+int Course::getID() const {
+    return id;
+}
+
+string Course::getName() const {
+    return name;
+}
+
+int Course::getCredits() const {
+    return credits;
+}
+
+void Course::setID(int newID) {
+    id = newID;
+}
+
+void Course::setName(string newName) {
+    name = newName;
+}
+
+void Course::setCredits(int newCredits) {
+    credits = newCredits;
+}
+
+istream& operator >> (istream& is, Course& course) {
+    is >> course.id >> course.name >> course.credits;
+    return is;
+}
+
+ostream& operator << (ostream& os, Course& course) {
+    os << "Course Name: " << course.name << "\n";
+    os << "Course ID: " << course.id << "\n";
+    os << "Course Credits: " << course.credits << "\n\n";
+    return os;
+}
---
+++ Final/Q2/Course.hpp
@@ -0,0 +1,31 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Course {
+    private:
+        int id;
+        string name;
+        int credits;
+    public:
+        Course();
+        Course(int, string, int);
+        ~Course();
+
+        int getID() const;
+        string getName() const;
+        int getCredits() const;
+
+        void setID(int);
+        void setName(string);
+        void setCredits(int);
+
+        friend istream& operator >> (istream& is, Course& course);
+        friend ostream& operator << (ostream& os, Course& course);
+};
+
+#endif
