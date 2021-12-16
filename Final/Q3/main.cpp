4a299de3d4e9228b82a4b4c199c10e4ce8593bca
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-16 00:50:14 -0800

    Beginning to test code

--- Final/Q3/main.cpp
+++ Final/Q3/main.cpp
@@ -1,10 +1,39 @@
 #include "ProductionWorker.cpp"
 #include <iostream>
-#include <fstream>
+// #include <fstream>
 
 using namespace std;
 
-int main() {
+// void fillWorkers(int);
+// void printWorkers(int);
 
+// const int SIZE = 10;
+// ProductionWorker workers[SIZE];
+
+int main() {
+    // fillWorkers(SIZE);
+    // printWorkers(SIZE);
     return 0;
 }
+
+// void fillWorkers(int size) {
+//     ifstream ifs;
+//     ifs.open("C:\\Users\\alifa\\Documents\\CS2 Assignments\\assignments-alifayed1\\Final\\Q3\\employees.txt");
+
+//     if (ifs.fail()) {
+//         cerr << "Failed to open employees.txt\n";
+//         exit(0);
+//     }
+
+//     for(int i = 0; i < size; i++) {
+//         ifs >> workers[i];
+//     }
+
+//     ifs.close();
+// }
+
+// void printWorkers(int size) {
+//     for(int i = 0; i < size; i++) {
+//         cout << workers[i];
+//     }
+// }
