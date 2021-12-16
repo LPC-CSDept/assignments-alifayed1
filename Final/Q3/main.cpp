52bd2755aa87fb2e6477ae7b319d3f154fa02b74
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-16 01:01:39 -0800

    Expanding driver code for testing

--- Final/Q3/main.cpp
+++ Final/Q3/main.cpp
@@ -1,39 +1,39 @@
 #include "ProductionWorker.cpp"
 #include <iostream>
-// #include <fstream>
+#include <fstream>
 
 using namespace std;
 
-// void fillWorkers(int);
-// void printWorkers(int);
+void fillWorkers(int);
+void printWorkers(int);
 
-// const int SIZE = 10;
-// ProductionWorker workers[SIZE];
+const int SIZE = 10;
+ProductionWorker workers[SIZE];
 
 int main() {
-    // fillWorkers(SIZE);
-    // printWorkers(SIZE);
+    fillWorkers(SIZE);
+    printWorkers(SIZE);
     return 0;
 }
 
-// void fillWorkers(int size) {
-//     ifstream ifs;
-//     ifs.open("C:\\Users\\alifa\\Documents\\CS2 Assignments\\assignments-alifayed1\\Final\\Q3\\employees.txt");
+void fillWorkers(int size) {
+    ifstream ifs;
+    ifs.open("C:\\Users\\alifa\\Documents\\CS2 Assignments\\assignments-alifayed1\\Final\\Q3\\employees.txt");
 
-//     if (ifs.fail()) {
-//         cerr << "Failed to open employees.txt\n";
-//         exit(0);
-//     }
+    if (ifs.fail()) {
+        cerr << "Failed to open employees.txt\n";
+        exit(0);
+    }
 
-//     for(int i = 0; i < size; i++) {
-//         ifs >> workers[i];
-//     }
+    for(int i = 0; i < size; i++) {
+        ifs >> workers[i];
+    }
 
-//     ifs.close();
-// }
+    ifs.close();
+}
 
-// void printWorkers(int size) {
-//     for(int i = 0; i < size; i++) {
-//         cout << workers[i];
-//     }
-// }
+void printWorkers(int size) {
+    for(int i = 0; i < size; i++) {
+        cout << workers[i];
+    }
+}
