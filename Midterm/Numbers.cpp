e451fc0aebacc5748333e26c968766655e610fbd
alifayed1 <ahfayed@zonemail.clpccd.edu>
2021-12-16 16:32:05 -0800

    Added more member function implementations

--- Midterm/Numbers.cpp
+++ Midterm/Numbers.cpp
@@ -1,12 +1,12 @@
 #include "Numbers.hpp"
 
 template<typename T>
-Numbers::Numbers() {
+Numbers<T>::Numbers() {
     id = 0;
 }
 
 template<typename T>
-Numbers::Numbers(int setID, int randomElems) {
+Numbers<T>::Numbers(int setID, int randomElems) {
     id = setID;
     for(int i = 0; i < randomElems; i++) {
         int val = rand() % 100;
@@ -15,7 +15,34 @@ Numbers::Numbers(int setID, int randomElems) {
 }
 
 template<typename T>
-Numbers::~Numbers() {
+Numbers<T>::~Numbers() {
 
 }
 
+template<typename T>
+int Numbers<T>::getID() const {
+    return id;
+}
+
+template<typename T>
+vector<T> Numbers<T>::getValues() const {
+    return values;
+}
+
+template<typename T>
+void Numbers<T>::setID(int newID) {
+    id = newID;
+}
+
+template<typename T>
+void Numbers<T>::setValues(vector<T> newValues) {
+    values = newValues;
+}
+
+template<typename T>
+int Numbers<T>::numOfObjects = 0;
+
+template<typename T>
+int Numbers<T>::getNumObjects(){
+    return numOfObjects;
+}
